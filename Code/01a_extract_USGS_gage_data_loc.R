9a1fa34090d47a95145915cd7e788b1b3941a22d
Jane Rogosch <jrogosch@ttu.edu>
2021-12-10 09:43:38 -0600

    Code to get gage location and site number

---
+++ Code/01a_extract_USGS_gage_data_loc.R
@@ -0,0 +1,181 @@
+### Jane S. Rogosch
+### Created 19 March 2021
+### This code is to extract USGS discharge data from gages in xeric ecoregion
+
+### relevant links:
+# Freshwater ecoregions: https://feow.org/
+
+### Load libraries--------------------------------------------------------------
+# Note: some of these libraries may not be necessary
+# code copied from script to calculate Discrete Fast Fourier Transform 
+# on mean daily discharge data  for installing packages and dependencies 
+# originally from Metacommunity Project circa 2017,
+# so some of these libraries may not be necessary
+library(ggplot2)
+library(lmom)
+library(CircStats)
+
+## Load discharge package
+library(discharge)
+
+# If you do not have the package you can download it 
+# from here: https://sourceforge.net/projects/discharge/ (see documentation)
+library(dataRetrieval)
+### NOTE: cannot have dplyr library loaded, because also has a filter function
+
+##USGS code to get water year
+# rprofile_path = file.path(Sys.getenv("HOME"), ".Rprofile")
+# write('\noptions(repos=c(getOption(\'repos\'),
+#       CRAN=\'https://cloud.r-project.org\',
+#       USGS=\'https://owi.usgs.gov/R\'))\n',
+#       rprofile_path,
+#       append =  TRUE)
+# 
+# # cat('Your Rprofile has been updated to include GRAN.
+# #     Please restart R for changes to take effect.')
+#  install.packages("smwrBase")
+library(smwrBase)
+
+
+### Example code to extract one ------------------------------------------------------------------------------------------------------
+# using San Pedro River, Arizona (USGS station 09471000)
+# Now let's download data from USGS servers
+siteNumber <-"09471000"
+parameterCd <- "00060" # this is discharge
+rawDailyData <-readNWISdv(siteNumber, parameterCd, "1996-10-01","2015-09-30")
+head(rawDailyData)
+rawDailyData <- rawDailyData[,3:4]
+class(rawDailyData$Date) # needs to be class 'Date', if not run next line
+#rawDailyData$Date<-as.Date(rawDailyData$Date, "%m/%d/%Y")
+head(rawDailyData) # OK
+
+# Convert raw data to 'asStreamflow' object
+SANPEDRO <- asStreamflow(rawDailyData, river.name="San Pedro River, AZ")
+
+dim(SANPEDRO$data)[1]
+summary(SANPEDRO)
+
+### To get gage site numbers and other info by state, modify URL to have correct state ID (e.g. UT)---------------
+# https://waterservices.usgs.gov/nwis/iv?format=rdb&stateCd=UT&modifiedSince=PT30M
+# data exported from each webpage to text file in Data>Gage_list_USA
+
+# Open files
+AZ_gages_long <- read.delim("Data/Gage_list_USA/AZ_gages.txt",  header = F) 
+head(AZ_gages_long)
+
+CA_gages_long <- read.delim("Data/Gage_list_USA/CA_gages.txt",  header = F) 
+head(CA_gages_long)
+
+CO_gages_long <- read.delim("Data/Gage_list_USA/CO_gages.txt",  header = F) 
+head(CO_gages_long)
+
+NM_gages_long <- read.delim("Data/Gage_list_USA/NM_gages.txt",  header = F) 
+head(NM_gages_long)
+
+NV_gages_long <- read.delim("Data/Gage_list_USA/NV_gages.txt",  header = F) 
+head(NV_gages_long)
+
+TX_gages_long <- read.delim("Data/Gage_list_USA/TX_gages.txt",  header = F) 
+head(TX_gages_long)
+
+UT_gages_long <- read.delim("Data/Gage_list_USA/UT_gages.txt",  header = F) 
+head(UT_gages_long)
+
+ID_gages_long <- read.delim("Data/Gage_list_USA/ID_gages.txt", header = F)
+head(ID_gages_long)
+
+OR_gages_long <- read.delim("Data/Gage_list_USA/OR_gages.txt", header = F)
+head(ID_gages_long)
+
+WY_gages_long <- read.delim("Data/Gage_list_USA/WY_gages.txt", header = F)
+head(ID_gages_long)
+
+
+# Find lines with gage numbers
+AZ_gages_long[15,] # 108 sites in this file
+AZ_gages_long[16:123,]
+AZ_gages_long[16:117,]
+
+CA_gages_long[15,] # 369 sites 
+CA_gages_long[16:385,]
+CA_gages_long[16:304,]
+
+CO_gages_long[15,] # 213 sites 
+CO_gages_long[16:229,]
+CO_gages_long[16:184,]
+
+NM_gages_long[15,] # 97 sites 
+NM_gages_long[16:113,]
+NM_gages_long[19:103,]
+
+NV_gages_long[15,] # 97 sites
+NV_gages_long[16:113,]
+NV_gages_long[16:97,]
+
+TX_gages_long[15, ] # 571 sites
+TX_gages_long[16:587,]
+TX_gages_long[16:539,]
+
+UT_gages_long[15,] # 76 sites in this file
+UT_gages_long[16:92,]
+
+ID_gages_long[15, ] #110 sites
+ID_gages_long[16:125, ]
+
+OR_gages_long[15, ] #224 sites
+OR_gages_long[16:239, ]
+
+WY_gages_long[15, ] #45 sites
+WY_gages_long[16:60, ]
+
+# Get gage site numbers
+AZ_siteNumbers <- substring(AZ_gages_long[16:117,], 11, 18)
+CA_siteNumbers <- substring(CA_gages_long[16:304,], 11, 18)
+CO_siteNumbers <- substring(CO_gages_long[16:184,], 11, 18)
+NM_siteNumbers <- substring(NM_gages_long[19:103,], 11, 18)
+NV_siteNumbers <- substring(NV_gages_long[16:97,], 11, 18)
+TX_siteNumbers <- substring(TX_gages_long[16:539,], 11, 18)
+UT_siteNumbers <- substring(UT_gages_long[16:86,], 11, 18)
+ID_siteNumbers <- substring(ID_gages_long[16:125,], 11, 18)
+OR_siteNumbers <- substring(OR_gages_long[16:239,], 11, 18)
+WY_siteNumbers <- substring(WY_gages_long[16:60,], 11, 18)
+
+
+# Fix Texas site numbers
+TX_siteNumbers[c(332,333,340,341,345,347,518)]
+TX_gages_long[c(332,333,340,341,345,347,518)+15,]
+TX_siteNumbers_b <- substring(TX_gages_long[c(332,333,340,341,345,347,518)+15,], 12, 19)
+TX_siteNumbers[c(332,333,340,341,345,347,518)] <- replace(TX_siteNumbers[c(332,333,340,341,345,347,518)], 
+                                                      1:7,TX_siteNumbers_b)
+
+# Get site info and lat/long for each gage
+siteINFO_state <- readNWISsite(TX_siteNumbers) #AZ, CA, CO, NM, NV, UT are ok
+
+USA_siteNumbers <- c(AZ_siteNumbers, CA_siteNumbers, CO_siteNumbers, NM_siteNumbers, 
+                     NV_siteNumbers, TX_siteNumbers, UT_siteNumbers, ID_siteNumbers,
+                     OR_siteNumbers, WY_siteNumbers)
+
+siteINFO <- readNWISsite(USA_siteNumbers)
+
+# Find what data is available
+dailyDataAvailable <- whatNWISdata(siteNumber = USA_siteNumbers, service = "dv", statCd="00003") 
+readNWISsite
+  # duplicates for sites with site numbers that are longer than 8 characters plus if there are multiple parameters
+  # want columns dec_lat_va and dec_long_va
+
+# Get specific info from gages
+# Common USGS Parameter Codes (parm_cd)
+# 00060 is discharge (cubic ft per sec)
+# 00010 is temperature
+# 00400 is pH
+library(dplyr)
+gageLocation <- distinct(dailyDataAvailable, x = site_no, .keep_all = TRUE)
+str(gageLocation)
+gageLocation <- as.matrix(gageLocation)
+str(gageLocation)
+head(gageLocation)
+
+write.csv(gageLocation, file = "Output/Gage_location_USA.csv")
+# Get list of gages (siteNumbers) that are near (xx km) a fish sampling location only
+
+
---
+++ Code/01b_get_AUS_gage_loc.R
@@ -0,0 +1,102 @@
+### Jane S. Rogosch
+### Created 08 Oct 2021
+### This code is to extract USGS discharge data from gages in xeric ecoregion
+
+### relevant links:
+# Freshwater ecoregions: https://feow.org/
+
+
+##################################################################################
+#### STEPS TO TAKE - WORK PLAN ###
+##################################################################################
+# 1. Get AUS gage data to be similar to USA gage data
+# 2. Get gages coordinates near fish surveys 
+# 3. Exploratory analysis in trends.
+
+###################################################################################################################
+### LOAD LIBRARIES ###
+####################################################################################################################
+### Relevant discharge libraries -----------------------------------------------------------------------
+# Use to look at Weighted regressions on times and discharge in streams to look at temporal variations
+# According to Freya this library with wrtds function separates signal from noise to get good idea of trends
+# Data hungry - needs long trend
+library(EGRET)
+  #function wrtds
+
+## Load discharge package for NAA and variance metrics
+library(discharge)
+
+# If you do not have the package you can download it 
+# from here: https://sourceforge.net/projects/discharge/ (see documentation)
+
+## To get USGS gage data in USA
+library(dataRetrieval)
+### NOTE: cannot have dplyr library loaded, because also has a filter function
+
+##USGS code to get water year
+# rprofile_path = file.path(Sys.getenv("HOME"), ".Rprofile")
+# write('\noptions(repos=c(getOption(\'repos\'),
+#       CRAN=\'https://cloud.r-project.org\',
+#       USGS=\'https://owi.usgs.gov/R\'))\n',
+#       rprofile_path,
+#       append =  TRUE)
+# 
+# # cat('Your Rprofile has been updated to include GRAN.
+# #     Please restart R for changes to take effect.')
+#  install.packages("smwrBase")
+## Functions like getting water year
+library(smwrBase)
+
+### Other libraries--------------------------------------------------------------
+# Note: some of these libraries may not be necessary
+# code copied from script to calculate Discrete Fast Fourier Transform 
+# on mean daily discharge data  for installing packages and dependencies 
+# originally from another project circa 2017,
+
+library(ggplot2)
+library(lmom)
+library(CircStats)
+
+
+
+test <- read.table("Data/GRDC_data/5712100_Q_Day.Cmd")
+head(test)
+test2 <- read.csv("Data/GRDC_data/5712100_Q_Day.Cmd")
+test3 <- read.csv("Data/GRDC_data/5110010_Q_Day.Cmd.txt")
+head(test2)
+# Get Gage locations from the .cmd files
+# This is how the USGS gage locations were extracted
+# ?grep
+# data[, grep("_va", names(data))] <- sapply(data[, grep("_va", 
+#                                                        names(data))], as.numeric)
+test[c(13:14),]
+test2[c(12:13),]
+t.string <- test3[c(12:13),]
+t.string2 <- test2[c(12:13),]
+
+substr(t.string,24,33)
+substr(t.string2,24,33) # Ok this works, so can make a loopty loo
+
+# Read in all Australia gage files from the GRDC_data folder
+# Example code: Adjust so that it takes the ones from "Data/GRDC_data" NOT entire working directory
+
+test3b <- read.delim("Data/GRDC_data/5110010_Q_Day.Cmd.txt")
+head(test3b); head(test3) #looks the same, hopefully behaves the same
+substr(test3b[c(12:13),], 24,33)
+test3
+# starting to edit code
+temp <- list.files(path="Data/GRDC_data/", pattern="_Q_Day.Cmd.txt")
+AUS_gage_files <- lapply(paste0("Data/GRDC_data/",temp), read.delim)
+str(AUS_gage_files)
+
+# Loop or try lapply
+test_list <- lapply(AUS_gage_files, function(x) substr(x[c(8,12:13), ], 24,33))
+test_list
+# read.csv("Data/GRDC_data/5405030_Q_Day.Cmd.txt") making sure is same values, even though short
+
+# Get from list to dataframe
+test.df <- data.frame(matrix(unlist(test_list), nrow=58, byrow=TRUE),
+                      stringsAsFactors=FALSE)
+colnames(test.df) <- c("site_no", "dec_lat_va", "dec_long_va")
+test.df
+write.csv(test.df, file = "Output/Gage_location_AUS.csv")
