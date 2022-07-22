49a5f24ecdc86556132582fcb91bb79563cc552d
freyarowland <frowland@usgs.gov>
2022-07-22 12:32:37 -0500

    Deleted blank lines

--- Code/DailyTemp.R
+++ Code/DailyTemp.R
@@ -80,8 +80,6 @@ tempanol <- ggplot(tempyear2, aes(x = year, y = anol, group = year)) +
 print(tempanol)
 
 
-
-
 # look at temp by site
 ggplot(tempyear2, aes(x = anol, y = as.factor(year), fill = stat(x))) +
   geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
