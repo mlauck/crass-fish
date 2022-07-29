ec0c9fb467e27ad7a61f22f3ecde91ba6285576a
freyarowland <frowland@usgs.gov>
2022-07-29 12:53:28 -0500

    Figure code for AUS site summer temp anomaly

--- Code/DailyTemp.R
+++ Code/DailyTemp.R
@@ -1,5 +1,4 @@
-# climate data
-
+# climate data for AUS
 dailytemp <- read.csv("Data/Avg_daily_temp_fish_sites_1979-2022_NAremoved.csv", header = TRUE)
 
 # libraries
@@ -43,53 +42,93 @@ tempUSE <- filter(tempUSE, year != 2022)
 # load(file = 'Data/tempUSE.rda')
 
 ## make average monthly temperature by site
-tempmonth <- tempUSE %>%
-  group_by(Site, year, month) %>%
+## for Dec-Feb
+tempsummer <- tempUSE %>%
+  group_by(Site, decimalLatitude, year) %>%
+  filter(month == 12 | month == 1 | month == 2) %>%
   summarise(avgTemp = mean(TempC, na.rm = TRUE),
             sdTemp = sd(TempC, na.rm = TRUE),
             n = n())
 
-# temp year
-tempyear <- tempUSE %>%
-  group_by(Site, year) %>%
+## for Nov-Feb
+tempsummer <- tempUSE %>%
+  group_by(Site, decimalLatitude, year) %>%
+  filter(month == 11 | month == 12 | month == 1 | month == 2) %>%
   summarise(avgTemp = mean(TempC, na.rm = TRUE),
             sdTemp = sd(TempC, na.rm = TRUE),
             n = n())
 
-# All time average annual temp C
-tempall <- tempUSE %>%
+## Nov only (start of summer -- is it getting earlier?)
+tempnov <- tempUSE %>%
+  group_by(Site, decimalLatitude, year) %>%
+  filter(month == 11) %>%
+  summarise(avgTemp = mean(TempC, na.rm = TRUE),
+            sdTemp = sd(TempC, na.rm = TRUE),
+            n = n())
+
+# # temp year
+# tempyear <- tempUSE %>%
+#   group_by(Site, year) %>%
+#   summarise(avgTemp = mean(TempC, na.rm = TRUE),
+#             sdTemp = sd(TempC, na.rm = TRUE),
+#             n = n())
+
+# All time average summer temp C
+tempall <- tempsummer %>%
   group_by(Site) %>%
-  summarize(overallavg = mean(TempC, na.rm = TRUE))
+  summarize(overallavg = mean(avgTemp, na.rm = TRUE))
+
+# All time Nov temp C
+tempall <- tempnov %>%
+  group_by(Site) %>%
+  summarize(overallavg = mean(avgTemp, na.rm = TRUE))
 
 # merge overall avg with annual average
-tempyear2 <- left_join(tempyear, tempall, by = "Site")
+tempsummer2 <- left_join(tempsummer, tempall, by = "Site")
+tempnov2 <- left_join(tempnov, tempall, by = "Site")
 
 # calculate annual anomaly
-tempyear2$anol <- tempyear2$overallavg - tempyear2$avgTemp
+tempsummer2$anol <- tempsummer2$overallavg - tempsummer2$avgTemp
+tempnov2$anol <- tempnov2$overallavg - tempnov$avgTemp
 
 ## plot temp anomaly
-tempanol <- ggplot(tempyear2, aes(x = year, y = anol, group = year)) +
+tempanol <- ggplot(tempsummer2, aes(x = year, y = anol, group = year)) +
   # geom_point(pch = 21, fill = "gray50", alpha = 0.5) +
   geom_boxplot() +
   theme_classic(base_size = 14) +
   theme(panel.background = element_rect(fill = "white", colour = "grey50")) +
   xlab("Year") +
-  ylab("Annual temperature anomaly (C)") +
-  ggtitle("Annual temperature anomaly") +
+  ylab("Summer temperature anomaly (°C)") +
+  ggtitle("Annual summer temperature anomaly") +
   geom_hline(yintercept = 0, color = "red", linetype = "dotted", size = 1)
 print(tempanol)
 
 
 # look at temp by site
-ggplot(tempyear2, aes(x = anol, y = as.factor(year), fill = stat(x))) +
+ggplot(tempsummer2, aes(x = anol, y = as.factor(year), fill = stat(x))) +
   geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
   scale_fill_viridis_c(name = "Temp anomaly", option = "C") +
   scale_color_viridis_c(name = "Temp anomaly", option = "C") +
   ylab("Year") +
-  xlab("Average annual temperature anomaly") +
+  xlab("Average summer temperature anomaly for Nov-Feb") +
   labs(
-    title = 'Annual temperature averages',
-    subtitle = 'Mean temperature anomaly (C) from 1979-2021'
+    title = 'Australia summer temperature averages',
+    subtitle = 'Mean temperature anomaly (°C) from 1979-2021'
+  ) +
+  theme_ridges(font_size = 14, grid = FALSE) + 
+  theme(axis.title.y = element_blank()) +
+  geom_vline(xintercept = 0, linetype = "dotted", size = 1)
+
+## only November temp anomalies
+ggplot(tempnov2, aes(x = anol, y = as.factor(year), fill = stat(x))) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
+  scale_fill_viridis_c(name = "Temp anomaly", option = "C") +
+  scale_color_viridis_c(name = "Temp anomaly", option = "C") +
+  ylab("Year") +
+  xlab("Temperature anomaly for November") +
+  labs(
+    title = 'Australia November temperatures',
+    subtitle = 'Mean temperature anomaly (°C) from 1979-2021'
   ) +
   theme_ridges(font_size = 14, grid = FALSE) + 
   theme(axis.title.y = element_blank()) +
