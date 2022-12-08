ad58d8add8611b28caa37ea3d6e6bf7f6cbcc1d6
freyarowland <frowland@usgs.gov>
2022-12-08 14:24:14 -0600

    Making some figures to look at 30+ yr fish data

--- Code/Site30yrsplus.R
+++ Code/Site30yrsplus.R
@@ -16,13 +16,45 @@ library(stringr)
 library(ggplot2)
 library(ggpubr)
 library(viridis)
+library(ggridges)
 
 summary(longfish)
-summary <- longfish %>%
-  group_by(year, hexID) %>%
-  summarize(speciescount = count(species, na.rm = TRUE))
 
-ggplot(aes(x = year, y = species, fill = hexID), data = longfish) +
+# species per site
+longfish %>%                                        # Specify data frame
+  group_by(year, hexID) %>%                         # Specify group indicator
+  summarise_at(vars(species),              # Specify column
+               list(name = count))                # Specify function
+
+ggplot(aes(x = species, y = year, fill = stat(x)), data = longfish) +
   geom_point(pch = 21) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.)
   scale_fill_viridis_d() +
   theme_bw()
+  
+  TQPlot2 <- ggplot(data = longfish, aes(x = year, y = species, group = hexID)) +
+    scale_fill_viridis_c(option = "magma", name = "year") +
+    geom_col(aes(fill = year)) +
+    theme_bw(base_size = 8) +
+    # geom_hline(yintercept = 1,
+    #            color = "black",
+    #            linetype = "dashed") +
+    xlab("Number of times detected") +
+    ylab("Species") +
+    theme(
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    ) +
+    ggtitle("Species presence in data") +
+    facet_wrap(~hexID)
+  print(TQPlot2)
+
+library(tidyverse)
+rich <- longfish %>% 
+    group_by(hexID, year) %>% 
+    # summarise_all(sum) %>%
+    # ungroup %>% 
+    mutate(Richness = count(species)) %>%
+    select(hexID, year, Richness)
