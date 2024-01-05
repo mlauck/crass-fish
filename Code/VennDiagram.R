d5eed3c417777f9c0cab484aafdee93365c484e7
Freya Rowland <frowland@usgs.gov>
2024-01-05 16:30:28 -0600

    Work more on ridge code. Maybe.

--- Code/VennDiagram.R
+++ Code/VennDiagram.R
@@ -1,52 +1,10 @@
-# Venn diagram
+# Venn diagrams and danger fish vs. rest
 
-# # devtools install
-# if (!require(devtools)) install.packages("devtools")
-# devtools::install_github("yanlinlin82/ggvenn")
-# 
-# # library
-# library(devtools)
-# library(ggplot2)
-# library(dplyr)
-# 
-# set.seed(20190708)
-# genes <- paste("gene",1:1000,sep="")
-# x <- list(
-#   A = sample(genes,300), 
-#   B = sample(genes,525), 
-#   C = sample(genes,440),
-#   D = sample(genes,350)
-# )
-# 
-# # Change category names
-# # Change fill color
-# display_venn(
-#   x,
-#   category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4"),
-#   fill = c("#999999", "#E69F00", "#56B4E9", "#009E73")
-# )
-# 
-# 
-# # Further customization
-# display_venn(
-#   x,
-#   category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4"),
-#   # Circles
-#   lwd = 2,
-#   lty = 'blank',
-#   fill = c("#999999", "#E69F00", "#56B4E9", "#009E73"),
-#   # Numbers
-#   cex = .9,
-#   fontface = "italic",
-#   # Set names
-#   cat.cex = 1,
-#   cat.fontface = "bold",
-#   cat.default.pos = "outer",
-#   cat.dist = c(0.055, 0.055, 0.1, 0.1)
-# )
+# libraries
+library(ggplot2)
+library(ggvenn)
 
 
-# try to make a list with arid data
 # load traits data
 traits <- read.csv("Data/allaridtraits.csv", header = TRUE)
 
@@ -54,8 +12,10 @@ traits <- read.csv("Data/allaridtraits.csv", header = TRUE)
 traits$danger <- NA
 traits$danger <-
   ifelse(traits$IUCNstatus == "LC" |
-           traits$IUCNstatus == "NE"|
-           traits$IUCNstatus == "DD", 0, 1)
+           traits$IUCNstatus == "NE" |
+           traits$IUCNstatus == "DD",
+         0,
+         1)
 head(traits)
 
 # only danger
@@ -68,6 +28,7 @@ rest <- traits %>%
 
 
 # make histograms comparing
+# length ----
 length <- ggplot(rest, aes(x = maxtl_cm), fill = "darkblue") +
   geom_histogram(binwidth = 5) +
   xlab("Maximum total length (cm)") +
@@ -81,7 +42,39 @@ length <- ggplot(rest, aes(x = maxtl_cm), fill = "darkblue") +
   scale_x_continuous(limits = c(0, 200))
 length
 
+# danger fish are smaller!
+ggplot(aes(y = maxtl_cm, x = danger, group = danger), data = traits) +
+  geom_boxplot() +
+  theme_bw(base_size = 14)
+summary(lm(maxtl_cm ~ danger, traits))
 
+# try a ggridge plot for kicks
+traits %>%
+  mutate(IUCNstatus = reorder(c("LC", "NE", "VU", "EN", "CR", "EX"))) %>%
+  ggplot(aes(y = IUCNstatus)) +
+  geom_density_ridges(
+    aes(x = maxtl_cm, fill = paste(YearFct, Option)), 
+    alpha = .8, color = "white", from = 0, to = 100
+  ) +
+  labs(
+    x = "Vote (%)",
+    y = "Election Year",
+    title = "Indy vs Unionist vote in Catalan elections",
+    subtitle = "Analysis unit: municipalities (n = 949)",
+    caption = "Marc Belzunces (@marcbeldata) | Source: Idescat"
+  ) +
+  scale_y_discrete(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_fill_cyclical(
+    breaks = c("1980 Indy", "1980 Unionist"),
+    labels = c(`1980 Indy` = "Indy", `1980 Unionist` = "Unionist"),
+    values = c("#ff0000", "#0000ff", "#ff8080", "#8080ff"),
+    name = "Option", guide = "legend"
+  ) +
+  coord_cartesian(clip = "off") +
+  theme_ridges(grid = FALSE)
+
+# trophic level ----
 TLplot <- ggplot(rest, aes(x = FoodTroph), fill = "darkblue") +
   geom_histogram(binwidth = 0.2) +
   xlab("Trophic level estimate") +
@@ -94,7 +87,13 @@ TLplot <- ggplot(rest, aes(x = FoodTroph), fill = "darkblue") +
   theme_bw(base_size = 14)
 TLplot
 
+# danger fish do not differ in diet
+ggplot(aes(y = FoodTroph, x = danger, group = danger), data = traits) +
+  geom_boxplot() +
+  theme_bw(base_size = 14)
+summary(lm(FoodTroph ~ danger, traits))
 
+# longevity ----
 ageplot <- ggplot(rest, aes(x = longevity), fill = "darkblue") +
   geom_histogram(binwidth = 5) +
   xlab("Maximum longevity (yrs)") +
@@ -104,9 +103,15 @@ ageplot <- ggplot(rest, aes(x = longevity), fill = "darkblue") +
     fill = "gold",
     binwidth = 5
   ) +
-  scale_x_continuous(limits = c(0,110)) +
+  scale_x_continuous(limits = c(0, 110)) +
   theme_bw(base_size = 14)
 ageplot
+
+# danger fish do not differ in longevity
+ggplot(aes(y = longevity, x = danger, group = danger), data = traits) +
+  geom_boxplot() +
+  theme_bw(base_size = 14)
+summary(lm(log(longevity) ~ danger, traits))
 
 # combined traits
 arid_v_danger_hist <- ggpubr::ggarrange(
@@ -122,51 +127,26 @@ arid_v_danger_hist <- ggpubr::ggarrange(
 )
 arid_v_danger_hist
 
-ggsave(arid_v_danger_hist, filename = "figures/traithistograms_onlyarid.png", dpi = 300, height = 12, width = 5)
+ggsave(
+  arid_v_danger_hist,
+  filename = "figures/traithistograms_onlyarid.png",
+  dpi = 300,
+  height = 12,
+  width = 5
+)
 
+
+## Make Venn diagrams
 # select only the species names and IUCN reasons
-IUCN <- subset(dangerfish, select = c (GenusSpecies, list1, list2, list3, list4, list5))
-
-# # make vectors for each listing reason
-# IUCN1 <- IUCN %>%
-#   group_by(GenusSpecies) %>%
-#   filter(list1 == TRUE) %>%
-#   select(GenusSpecies)
-# 
-# IUCN2 <- IUCN %>%
-#   group_by(GenusSpecies) %>%
-#   filter(list2 == TRUE) %>%
-#   select(GenusSpecies)
-# 
-# IUCN3 <- IUCN %>%
-#   group_by(GenusSpecies) %>%
-#   filter(list3 == TRUE) %>%
-#   select(GenusSpecies)
-# 
-# IUCN4 <- IUCN %>%
-#   group_by(GenusSpecies) %>%
-#   filter(list4 == TRUE) %>%
-#   select(GenusSpecies)
-
-# # make list
-# x <- list(IUCN1, IUCN2, IUCN3, IUCN4)
-# 
-# list_venn <- list(
-#   A = IUCN1[,1], 
-#   B = IUCN2[,1], 
-#   C = IUCN3[,1],
-#   D = IUCN4[,1]
-# )
+IUCN <-
+  subset(dangerfish,
+         select = c (GenusSpecies, list1, list2, list3, list4, list5))
 
 
-# libraries
-library(ggplot2)
-library(ggvenn)
 
 # ggvenn
-# make data_venn
 str(IUCN)
-IUCN$list1 <- IUCN$Habitat
+# IUCN$list1 <- IUCN$Habitat
 
 IUCN$GenusSpecies <- as.factor(IUCN$GenusSpecies)
 ggplot(IUCN, aes(
@@ -175,20 +155,11 @@ ggplot(IUCN, aes(
   C = list3,
   D = list4
 )) +
-  geom_venn(fill_color = c("#ffffcc",
-    "#a1dab4",
-    "#41b6c4",
-    "#225ea8"),
-    set_names = c("Habitat","Small range", "Disease", "Other factors")) +
+  geom_venn(
+    fill_color = c("#ffffcc",
+                   "#a1dab4",
+                   "#41b6c4",
+                   "#225ea8"),
+    set_names = c("Habitat", "Small range", "Disease", "Other factors")
+  ) +
   theme_void()
-
-# library("ggVennDiagram")
-# ggVennDiagram(IUCN, aes(
-#   A = list1,
-#   B = list5,
-#   C = list3,
-#   D = list4
-# )) +
-#   # category.names = c("Habitat", "Small range", "Disease", "Other factors") +
-#   geom_venn() +
-#   theme_void()
