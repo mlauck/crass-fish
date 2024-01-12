139328c19c66cb367f00b5d7ffc0a2691a6e328d
Freya Rowland <frowland@usgs.gov>
2024-01-12 14:29:35 -0600

    Commented out old attempts at phylogeny

--- Code/FishPhyloMaker.R
+++ Code/FishPhyloMaker.R
@@ -269,220 +269,220 @@ print(p2)
 
 
 
-## another attempt
-## Load needed packages for this blogpost
-library(phytools)
-library(ggtree)
-library(tidyverse)
-library(scico)
-library(viridisLite)
-
-## Load anole tree
-anole.tree <- read.tree("http://www.phytools.org/eqg2015/data/anole.tre")
-
-## Load anole trait data, extract snout-vent-length (svl) as named vector
-# svl <- read_csv("http://www.phytools.org/eqg2015/data/svl.csv") %>%
-#   mutate(svl = set_names(svl, species)) %>%
-#   pull(svl)
-
-library(magrittr)
-
-stat <- allarid %>%
-  mutate(IUCN = set_names(IUCNstatus, X)) %>%
-  pull(IUCN)
-
-library(ggimage)
-library(ggtree)
-library(TDbook)
-
-# load `tree_boots`, `df_tip_data`, and `df_inode_data` from 'TDbook'
-p <- ggtree(phylo, layout = "circular") %<+% allarid + xlim(-.1, 4)
-p2 <- p + geom_tiplab(offset = .6, hjust = .5) +
-  geom_tippoint(aes(color = endemic)) + 
-  theme(legend.position = "right") + 
-  geom_tiplab2(size = 2)
-
-p <- ggtree(phylo, layout = "circular") + geom_tiplab2(size = 3)
-p
-
-p2 %<+% df_inode_data + 
-  geom_label(aes(label = vernacularName.y, fill = posterior)) + 
-  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(3, "YlGnBu"))
-
-p %<+% danger2 +
-  geom_tiplab2()
-
-# Plot with default color scheme
-# contmap_obj <- contMap(anole.tree, svl, plot = FALSE)
-contmap_obj <- contMap(phylo, stat, plot = FALSE)
-
-plot(
-  contmap_obj, 
-  type="fan", 
-  legend = 0.7*max(nodeHeights(anole.tree)),
-  fsize = c(0.5, 0.7))
-
-## merge data
-# get danger fish
-head(allarid)
-
-library(phyloseq)
-library(ggjoy)
-library(dplyr)
-library(ggtree)
-
-mergedGP <- merge_samples(phylo, allarid)
-mergedGP <- rarefy_even_depth(mergedGP,rngseed=394582)
-mergedGP <- tax_glom(mergedGP,"Order") 
-
-
-PR.PG <- plot1 + geom_hilight(data = df.phylo, aes(node = node.number, fill = Fam.names), 
-                              alpha = .6) +
-  scale_fill_viridis(discrete = T, name = "Family names")
-
-
-
-
-
-
-
-
-
-
-
-## plot tree
-library(ape)
-library(phytools)
-
-# read data from http://fishtreeoflife.org/taxonomy
-my.tree <- read.tree('Data/fishorder_skeletal.tre')
-
+# ## another attempt
+# ## Load needed packages for this blogpost
+# library(phytools)
+# library(ggtree)
+# library(tidyverse)
+# library(scico)
+# library(viridisLite)
 # 
-# pairwise taxa-taxa distance matrix
-d <- cophenetic(my.tree)
-d
-
-
-# compute total tree height
-h <- max(nodeHeights(my.tree))
-
-
-# plot tree
-plot(my.tree, edge.width = 1)
-plot(my.tree, font = 1, cex = 0.45)
-
-# generate sampling points
-interval <- 50 # every 30 million years
-dd <- seq(h,0, by =-interval)
-
-# plot with millions of years
-plotTree(my.tree, mar = c(5.1, 4.1, 1.1, 1.1), cex = 0.35)
-axis(1)
-xlab("Millions of years")
-abline(v = dd, lty = "dotted", col = "grey")
-
-# trim a phylogeny
-## Orders
-# drop.tip
-no_data <- c("Acanthuriformes", "Alepocephaliformes",
-             "Argentiniformes", "Ateleopodiformes",
-             "Beryciformes", "Ephippiformes",
-             "Galaxiiformes", "Hiodontiformes",
-             "Istiophoriformes", "Lampridiformes",
-             "Lepidogalaxiiformes", "Myctophiformes",
-             "Notacanthiformes", "Pempheriformes",
-             "Percopsiformes", "Pholidichthyiformes",
-             "Polymixiiformes", "Spariformes",
-             "Stomiatiformes", "Stylephoriformes",
-             "Uranoscopiformes", "Zeiformes")
-plot(drop.tip(fish.tree, no_data))
-
-# pruned tree
-my.tree$tip.label
-
-# taxa to keep
-keep_taxa <- c("Polypteriformes", "Acipenseriformes",
-               "Lepisosteiformes", "Amiiformes",
-               "Elopiformes", "Anguilliformes",
-               "Albuliformes", "Osteoglossiformes",
-               "Clupeiformes", "Gonorynchiformes",
-               "Cypriniformes", "Gymnotiformes",
-               "Siluriformes", "Characiformes",
-               "Salmoniformes", "Esociformes",
-               "Osmeriformes", "Aulopiformes",
-               "Gadiformes", "Holocentriformes",
-               "Ophidiiformes", "Batrachoidiformes",
-               "Labriformes", "Tetraodontiformes",
-               "Lophiiformes", "Incertae_sedis_in_Eupercaria",
-               "Perciformes", "Centrarchiformes",
-               "Gobiiformes", "Kurtiformes",
-               "Syngnathiformes", "Pleuronectiformes",
-               "Incertae_sedis_in_Carangaria", 
-               "Carangiformes", "Mugiliformes",
-               "Incertae_sedis_in_Ovalentaria",
-               "Cichliformes", "Scombriformes",
-               "Atheriniformes", "Cyprinodontiformes",
-               "Beloniformes", "Synbranchiformes")
-
-keep_taxa2 <- taxon_data2$Taxon_data_FishPhyloMaker$o
-keep_taxa3 <- na.omit(keep_taxa2)
-keep_taxa3 <- unique(keep_taxa3)
-
-# check to see which taxa are being removed
-setdiff(my.tree$tip.label, keep_taxa3)      
-
-# remove
-remove_taxa <- setdiff(my.tree$tip.label, keep_taxa3)
-
-# pruned tree
-pruned_tree <- drop.tip(my.tree, remove_taxa)
-pruned_tree$tip.label
-
-plot(pruned_tree)
-
-as.data.frame(pruned_tree$tip.lab)
-
-# get tree statistics ----
-summary(pruned_tree)
-
-# how to plot with thiaminase presence/absence ----
-fish.tree<-read.tree("data/fishorder_skeletal.tre")
-fish.data<-read.csv("data/OrderPresAbsNA.csv",row.names=1)
-fish.data <- read.csv("Data/orderdanger.csv")
-
-fmode<-as.factor(setNames(fish.data[,1],rownames(fish.data)))
-fmode<-setNames(orderdanger$prop.dang,nm = as.factor(orderdanger$o))
-dotTree(drop.tip(fish.tree, no_data),
-        fmode,
-        colors=setNames(c("white","red"),
-                        c("absent","present")),
-        legend = FALSE,
-        edge.width = 1,
-        ftype="i",
-        fsize=0.55,
-        mar = c(5.1, 4.1, 1.1, 1.1), cex = 0.55)
-axis(1)
-
-## anova ----
-# won't work because response is yes/no
-phylANOVA(tree = pruned_tree, x= pruned_tree$tip.label, y = fmode, nsim = 200, posthoc = TRUE, p.adj = "holm")
-
-# make simulation of probability of thiaminase ----
-# equal probability across the whole tree? LOL
-# thia.trees<-make.simmap(drop.tip(fish.tree, no_data),
-#                         fmode,
-#                         nsim=200)
-# obj<-densityMap(thia.trees,states=c("present","absent"),plot=FALSE)
-# plot(obj,lwd=4,outline=TRUE,fsize=c(0.7,0.9),legend=50)
-
-# stochastic mapping ----
-smap.trees <- make.simmap(pruned_tree, fmode, 
-                          model = "ER", nsim = 500)
-summary(smap.trees)
-
-pdf('figures/order_phylogeny.pdf', height = 8, width = 6)
-cols <- setNames(c("black", "white"), c("threatened", "not a concern"))
-plot(summary(smap.trees), colors = cols)
-legend("topleft", c("present", "absent"),
-       pch = 21, pt.bg = cols, pt.cex = 2)
-dev.off()
+# ## Load anole tree
+# anole.tree <- read.tree("http://www.phytools.org/eqg2015/data/anole.tre")
+# 
+# ## Load anole trait data, extract snout-vent-length (svl) as named vector
+# # svl <- read_csv("http://www.phytools.org/eqg2015/data/svl.csv") %>%
+# #   mutate(svl = set_names(svl, species)) %>%
+# #   pull(svl)
+# 
+# library(magrittr)
+# 
+# stat <- allarid %>%
+#   mutate(IUCN = set_names(IUCNstatus, X)) %>%
+#   pull(IUCN)
+# 
+# library(ggimage)
+# library(ggtree)
+# library(TDbook)
+# 
+# # load `tree_boots`, `df_tip_data`, and `df_inode_data` from 'TDbook'
+# p <- ggtree(phylo, layout = "circular") %<+% allarid + xlim(-.1, 4)
+# p2 <- p + geom_tiplab(offset = .6, hjust = .5) +
+#   geom_tippoint(aes(color = endemic)) + 
+#   theme(legend.position = "right") + 
+#   geom_tiplab2(size = 2)
+# 
+# p <- ggtree(phylo, layout = "circular") + geom_tiplab2(size = 3)
+# p
+# 
+# p2 %<+% df_inode_data + 
+#   geom_label(aes(label = vernacularName.y, fill = posterior)) + 
+#   scale_fill_gradientn(colors = RColorBrewer::brewer.pal(3, "YlGnBu"))
+# 
+# p %<+% danger2 +
+#   geom_tiplab2()
+# 
+# # Plot with default color scheme
+# # contmap_obj <- contMap(anole.tree, svl, plot = FALSE)
+# contmap_obj <- contMap(phylo, stat, plot = FALSE)
+# 
+# plot(
+#   contmap_obj, 
+#   type="fan", 
+#   legend = 0.7*max(nodeHeights(anole.tree)),
+#   fsize = c(0.5, 0.7))
+# 
+# ## merge data
+# # get danger fish
+# head(allarid)
+# 
+# library(phyloseq)
+# library(ggjoy)
+# library(dplyr)
+# library(ggtree)
+# 
+# mergedGP <- merge_samples(phylo, allarid)
+# mergedGP <- rarefy_even_depth(mergedGP,rngseed=394582)
+# mergedGP <- tax_glom(mergedGP,"Order") 
+# 
+# 
+# PR.PG <- plot1 + geom_hilight(data = df.phylo, aes(node = node.number, fill = Fam.names), 
+#                               alpha = .6) +
+#   scale_fill_viridis(discrete = T, name = "Family names")
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# ## plot tree
+# library(ape)
+# library(phytools)
+# 
+# # read data from http://fishtreeoflife.org/taxonomy
+# my.tree <- read.tree('Data/fishorder_skeletal.tre')
+# 
+# # 
+# # pairwise taxa-taxa distance matrix
+# d <- cophenetic(my.tree)
+# d
+# 
+# 
+# # compute total tree height
+# h <- max(nodeHeights(my.tree))
+# 
+# 
+# # plot tree
+# plot(my.tree, edge.width = 1)
+# plot(my.tree, font = 1, cex = 0.45)
+# 
+# # generate sampling points
+# interval <- 50 # every 30 million years
+# dd <- seq(h,0, by =-interval)
+# 
+# # plot with millions of years
+# plotTree(my.tree, mar = c(5.1, 4.1, 1.1, 1.1), cex = 0.35)
+# axis(1)
+# xlab("Millions of years")
+# abline(v = dd, lty = "dotted", col = "grey")
+# 
+# # trim a phylogeny
+# ## Orders
+# # drop.tip
+# no_data <- c("Acanthuriformes", "Alepocephaliformes",
+#              "Argentiniformes", "Ateleopodiformes",
+#              "Beryciformes", "Ephippiformes",
+#              "Galaxiiformes", "Hiodontiformes",
+#              "Istiophoriformes", "Lampridiformes",
+#              "Lepidogalaxiiformes", "Myctophiformes",
+#              "Notacanthiformes", "Pempheriformes",
+#              "Percopsiformes", "Pholidichthyiformes",
+#              "Polymixiiformes", "Spariformes",
+#              "Stomiatiformes", "Stylephoriformes",
+#              "Uranoscopiformes", "Zeiformes")
+# plot(drop.tip(fish.tree, no_data))
+# 
+# # pruned tree
+# my.tree$tip.label
+# 
+# # taxa to keep
+# keep_taxa <- c("Polypteriformes", "Acipenseriformes",
+#                "Lepisosteiformes", "Amiiformes",
+#                "Elopiformes", "Anguilliformes",
+#                "Albuliformes", "Osteoglossiformes",
+#                "Clupeiformes", "Gonorynchiformes",
+#                "Cypriniformes", "Gymnotiformes",
+#                "Siluriformes", "Characiformes",
+#                "Salmoniformes", "Esociformes",
+#                "Osmeriformes", "Aulopiformes",
+#                "Gadiformes", "Holocentriformes",
+#                "Ophidiiformes", "Batrachoidiformes",
+#                "Labriformes", "Tetraodontiformes",
+#                "Lophiiformes", "Incertae_sedis_in_Eupercaria",
+#                "Perciformes", "Centrarchiformes",
+#                "Gobiiformes", "Kurtiformes",
+#                "Syngnathiformes", "Pleuronectiformes",
+#                "Incertae_sedis_in_Carangaria", 
+#                "Carangiformes", "Mugiliformes",
+#                "Incertae_sedis_in_Ovalentaria",
+#                "Cichliformes", "Scombriformes",
+#                "Atheriniformes", "Cyprinodontiformes",
+#                "Beloniformes", "Synbranchiformes")
+# 
+# keep_taxa2 <- taxon_data2$Taxon_data_FishPhyloMaker$o
+# keep_taxa3 <- na.omit(keep_taxa2)
+# keep_taxa3 <- unique(keep_taxa3)
+# 
+# # check to see which taxa are being removed
+# setdiff(my.tree$tip.label, keep_taxa3)      
+# 
+# # remove
+# remove_taxa <- setdiff(my.tree$tip.label, keep_taxa3)
+# 
+# # pruned tree
+# pruned_tree <- drop.tip(my.tree, remove_taxa)
+# pruned_tree$tip.label
+# 
+# plot(pruned_tree)
+# 
+# as.data.frame(pruned_tree$tip.lab)
+# 
+# # get tree statistics ----
+# summary(pruned_tree)
+# 
+# # how to plot with thiaminase presence/absence ----
+# fish.tree<-read.tree("data/fishorder_skeletal.tre")
+# fish.data<-read.csv("data/OrderPresAbsNA.csv",row.names=1)
+# fish.data <- read.csv("Data/orderdanger.csv")
+# 
+# fmode<-as.factor(setNames(fish.data[,1],rownames(fish.data)))
+# fmode<-setNames(orderdanger$prop.dang,nm = as.factor(orderdanger$o))
+# dotTree(drop.tip(fish.tree, no_data),
+#         fmode,
+#         colors=setNames(c("white","red"),
+#                         c("absent","present")),
+#         legend = FALSE,
+#         edge.width = 1,
+#         ftype="i",
+#         fsize=0.55,
+#         mar = c(5.1, 4.1, 1.1, 1.1), cex = 0.55)
+# axis(1)
+# 
+# ## anova ----
+# # won't work because response is yes/no
+# phylANOVA(tree = pruned_tree, x= pruned_tree$tip.label, y = fmode, nsim = 200, posthoc = TRUE, p.adj = "holm")
+# 
+# # make simulation of probability of thiaminase ----
+# # equal probability across the whole tree? LOL
+# # thia.trees<-make.simmap(drop.tip(fish.tree, no_data),
+# #                         fmode,
+# #                         nsim=200)
+# # obj<-densityMap(thia.trees,states=c("present","absent"),plot=FALSE)
+# # plot(obj,lwd=4,outline=TRUE,fsize=c(0.7,0.9),legend=50)
+# 
+# # stochastic mapping ----
+# smap.trees <- make.simmap(pruned_tree, fmode, 
+#                           model = "ER", nsim = 500)
+# summary(smap.trees)
+# 
+# pdf('figures/order_phylogeny.pdf', height = 8, width = 6)
+# cols <- setNames(c("black", "white"), c("threatened", "not a concern"))
+# plot(summary(smap.trees), colors = cols)
+# legend("topleft", c("present", "absent"),
+#        pch = 21, pt.bg = cols, pt.cex = 2)
+# dev.off()
