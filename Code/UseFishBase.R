43620e62e7f9d42f6378ae5fb09c9ff15d54674f
Freya Rowland <frowland@usgs.gov>
2024-03-15 15:54:42 -0500

    Update all arid fishes vs. freshwater

--- Code/UseFishBase.R
+++ Code/UseFishBase.R
@@ -19,7 +19,7 @@ streamfish <- fish.data %>%
 allfish <- left_join(streamfish, fish.est, by = "SpecCode")
 
 # load traits data
-allarid <- read.csv("Data/fish_traits.csv", header = TRUE)
+allarid <- read.csv("FinalData/Traits_MatchedWithOccurancePlusTrophic.csv", header = TRUE)
 # make a vector of fish names
 fish <- allarid[,1]
 unique(fish)
@@ -31,51 +31,75 @@ estaridfish <- estimate(fish) #MaxLengthTL, Troph, seTroph, TempPrefMin, TempPre
 
 
 # make histograms comparing
-length <- ggplot(allfish, aes(x = MaxLengthTL), fill = "darkblue") +
-  geom_histogram(binwidth = 5) +
+length <- ggplot(allfish, aes(x = MaxLengthTL)) +
+  geom_histogram(binwidth = 5,
+                 fill = "#1b0c41") +
   xlab("Maximum total length (cm)") +
   geom_histogram(
     data = allarid,
     aes(x = maxtl_cm),
-    fill = "gold",
+    fill = "#f7d13d",
     binwidth = 5
   ) +
+  ylab("Count") +
   theme_bw(base_size = 14) +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  ) +
   scale_x_continuous(limits = c(0, 200))
 length
 
 
-TLplot <- ggplot(allfish, aes(x = Troph), fill = "darkblue") +
-  geom_histogram(binwidth = 0.2) +
+TLplot <- ggplot(allfish, aes(x = Troph)) +
+  geom_histogram(binwidth = 0.2,
+                 fill = "#1b0c41") +
   xlab("Trophic level estimate") +
+  ylab("Count") +
   geom_histogram(
     data = estaridfish,
     aes(x = Troph),
-    fill = "gold",
+    fill = "#f7d13d",
     binwidth = 0.2
   ) +
-  theme_bw(base_size = 14)
+  theme_bw(base_size = 14) +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
 TLplot
 
 
-ageplot <- ggplot(allfish, aes(x = AgeMax), fill = "darkblue") +
-  geom_histogram(binwidth = 5) +
+ageplot <- ggplot(allfish, aes(x = AgeMax)) +
+  geom_histogram(binwidth = 5,
+                 fill = "#1b0c41") +
   xlab("Maximum longevity (yrs)") +
   geom_histogram(
     data = allarid,
     aes(x = longevity),
-    fill = "gold",
+    fill = "#f7d13d",
     binwidth = 5
   ) +
+  ylab("Count") +
   scale_x_continuous(limits = c(0,110)) +
-  theme_bw(base_size = 14)
+  theme_bw(base_size = 14) +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
 ageplot
 
 # combined traits
 combplot <- ggpubr::ggarrange(
-  ageplot,
   length,
   TLplot,
+  ageplot,
   nrow = 3,
   ncol = 1,
   align = "hv",
