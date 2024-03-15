53f0744cbcdb9d36ca0550f6948da9989ef6af07
Freya Rowland <frowland@usgs.gov>
2024-03-15 11:15:33 -0500

    Remade arid vs. all fishes figure

--- Code/IUCNvsArid.R
+++ Code/IUCNvsArid.R
@@ -1,6 +1,6 @@
 # Comparing IUCN lists of all fishes vs. arid fishes
 # FER
-# Last edit 6 May 2022
+# Last edit March 2024
 
 ## next week to do:
 # melt data into one file
@@ -18,7 +18,7 @@ library(data.table)
 
 # load data
 allIUCN <- read.csv("Data/IUCNassessments.csv", header = TRUE)
-allarid <- read.csv("FinalData/Traits_MatchedWithOccurrence.csv", header = TRUE)
+allarid <- read.csv("FinalData/Traits_MatchedWithOccurancePlusTrophic.csv", header = TRUE)
 
 # get trophic level
 fish <- allarid[,1]
@@ -31,40 +31,40 @@ allarid <- merge(allarid, food, by.x = "GenusSpecies", by.y = "Species")
 # make location a factor
 allarid$location2 <- as.factor(allarid$location2)
 
-# # save these data
-write.csv(allarid, "FinalData/Traits_MatchedWithOccurrence.csv")
+# # # save these data
+# write.csv(allarid, "FinalData/Traits_MatchedWithOccurrence.csv")
 
-# summarize the arid fish data
-summary <- allarid %>%
-  group_by(location2) %>%
-  summarise(
-    count = n(),
-    Trophic = mean(FoodTroph, na.rm = TRUE),
-    TrophicSD = sd(FoodTroph, na.rm = TRUE),
-    maxlength = mean(maxtl_cm, na.rm = TRUE),
-    maxlength_sd = sd(maxtl_cm, na.rm = TRUE),
-    benthic = length(which(benthic)),
-    surwcol = length(which(surwcol)),
-    nonfeed = length(which(nonfeed)),
-    algphyto = length(which(algphyto)),
-    macvascu = length(which(macvascu)),
-    detritus = length(which(detritus)),
-    invlvfsh = length(which(invlvfsh)),
-    fishcr = length(which(fshcrcrb)),
-    blood = length(which(blood)),
-    eggs = length(which(eggs)),
-    serial = length(which(serial)),
-    guarder = length(which(guarder)),
-    open.sub = length(which(open.substratum.spawner)),
-    broodhider = length(which(broodhider)),
-    live = length(which(livebearers)),
-    euryhaline = length(which(euryhaline)),
-    potandr = length(which(potanadr)),
-    slowcurr = length(which(slowcurr)),
-    modcurr = length(which(modcurr)),
-    fastcurr = length(which(fastcurr))
-)
-write.csv(summary, file = "data/fishtraitsummary.csv")
+# # summarize the arid fish data
+# summary <- allarid %>%
+#   group_by(location2) %>%
+#   summarise(
+#     count = n(),
+#     Trophic = mean(FoodTroph, na.rm = TRUE),
+#     TrophicSD = sd(FoodTroph, na.rm = TRUE),
+#     maxlength = mean(maxtl_cm, na.rm = TRUE),
+#     maxlength_sd = sd(maxtl_cm, na.rm = TRUE),
+#     benthic = length(which(benthic)),
+#     surwcol = length(which(surwcol)),
+#     nonfeed = length(which(nonfeed)),
+#     algphyto = length(which(algphyto)),
+#     macvascu = length(which(macvascu)),
+#     detritus = length(which(detritus)),
+#     invlvfsh = length(which(invlvfsh)),
+#     fishcr = length(which(fshcrcrb)),
+#     blood = length(which(blood)),
+#     eggs = length(which(eggs)),
+#     serial = length(which(serial)),
+#     guarder = length(which(guarder)),
+#     open.sub = length(which(open.substratum.spawner)),
+#     broodhider = length(which(broodhider)),
+#     live = length(which(livebearers)),
+#     euryhaline = length(which(euryhaline)),
+#     potandr = length(which(potanadr)),
+#     slowcurr = length(which(slowcurr)),
+#     modcurr = length(which(modcurr)),
+#     fastcurr = length(which(fastcurr))
+# )
+# write.csv(summary, file = "data/fishtraitsummary.csv")
 
 # make columns categories
 allIUCN$redListCategory <- as.factor(allIUCN$redlistCategory)
@@ -74,7 +74,7 @@ allarid$IUCNstatus <- as.factor(allarid$IUCNstatus)
 
 # make figure of the IUCN all fishes and status ----
 # summarize data by status out of 11211 species in database
-IUCN_df<- allIUCN %>%
+IUCN_df <- allIUCN %>%
   group_by(redListCategory) %>%
   summarise(n = n(),
             prop = n()/11211)
@@ -104,15 +104,14 @@ allarid$IUCNstatus <- recode_factor(allarid$IUCNstatus,
                                     EW = "Extinct in the Wild",
                                     CE = "Critically Endangered")
 
-# total fishes = length(!is.na(allarid$IUCNstatus)) = 428 - 12 in weird row = 416
+# total fishes = length(!is.na(allarid$IUCNstatus)) = 137
 arid_df <- allarid %>%
   group_by(IUCNstatus) %>%
   summarize(n = n(),
-            prop = n()/416)
+            prop = n()/137)
 
-arid_df2 <- arid_df[-10, ] # empty cells excluded in row 10
 
-arid_df2 <- as.data.frame(arid_df2)
+arid_df2 <- as.data.frame(arid_df)
 
 ## merge df together
 alldata <- rbind(IUCN_df2, arid_df2)
@@ -172,7 +171,7 @@ barplot <- ggplot(alldata, aes(x = IUCNstatus, y = prop, group = source)) +
     alpha = .9
   ) +
   scale_x_discrete(labels = function(x) str_wrap(x, width = 6)) +
-  ylab("proportion of all fishes") +
+  ylab("Proportion of all fishes") +
   xlab("IUCN status") +
   scale_fill_viridis_d(name = "Source",
                        begin = 0.7,
@@ -187,132 +186,132 @@ barplot <- ggplot(alldata, aes(x = IUCNstatus, y = prop, group = source)) +
   )
 print(barplot)
 
-# ggsave(
-#   device = "png",
-#   plot = barplot,
-#   filename = "figures/IUCN_v_arid.png",
-#   height = 4,
-#   width = 9,
-#   dpi = 300
-# )
-
-## first attempt at PCA ----
-# modeled after this: https://repositories.lib.utexas.edu/bitstream/handle/2152/94746/Perkin%20et%20al%202021.pdf?sequence=3
-library(tree)
-library(rpart)
-library(rpart.plot)
-
-# first filter data based on columns of interest
-names(allarid)
-
-aridCARTdat <- select(allarid, 
-                      c("GenusSpecies",
-                      "habitat",
-                      "location",
-                      "endemic",
-                      "AUSnative",
-                      "USAnative",
-                      "nonfeed",
-                      "algphyto",
-                      "macvascu",
-                      "detritus",
-                      "invlvfsh",
-                      "fshcrcrb",
-                      "blood",
-                      "eggs",
-                      "maxtl_cm",
-                      "guarder",
-                      "open.substratum.spawner",
-                      "broodhider",
-                      "livebearers",
-                      "sprgsubt",
-                      "lacustrine",
-                      "potanadr",
-                      "slowcurr",
-                      "modcurr",
-                      "fastcurr",
-                      "IUCNstatus",
-                      "FoodTroph.x"))
-str(aridCARTdat)
-
-# make many of these columns factors instead
-cols <- c("location", "endemic", "AUSnative", "USAnative",
-          "algphyto", "macvascu", "detritus", "invlvfsh",
-          "fshcrcrb", "blood", "eggs", "guarder", "open.substratum.spawner",
-          "broodhider", "livebearers", "sprgsubt", "lacustrine",
-          "potanadr", "slowcurr", "modcurr", "fastcurr")
-
-aridCARTdat %<>%
-  mutate_each_(funs(as.factor(.)),cols)
-# aridCARTdat %<>%
-#   mutate_each_(funs(as.logical(.)),cols)
-str(aridCARTdat)
-summary(aridCARTdat) # all looks good
-
-# ## to make CART ready elimate NAs
-aridCARTdat <- na.omit(aridCARTdat)
-
-## melt dataframe for food, current, and spawning
-# made a clunky ifelse statement because that's how I am
-aridCARTdat$spawning <- (ifelse(aridCARTdat$open.substratum.spawner == TRUE, "open.substratum",
-                               ifelse(aridCARTdat$broodhider == TRUE, "broodhider",
-                                      ifelse(aridCARTdat$livebearers == TRUE, "livebearers",
-                                             ifelse(aridCARTdat$guarder == TRUE, "guarder",
-                                                    NA)))))
-aridCARTdat$spawning <- as.factor(aridCARTdat$spawning)
-                    
-
-
-## make a new column for species in trouble
-setDT(aridCARTdat)
-aridCARTdat$dangerfish <-
-  aridCARTdat[, dangerfish := ifelse(IUCNstatus %in%
-                                       c("Endangered", 
-                                         "Extinct",
-                                         "Critically Endangered",
-                                         "Extinct in the Wild",
-                                         "Near Threatened",
-                                         "Vulnerable"), 1, 0)]
-
-# aridCARTdat$dangerfish <-
-#   ifelse(
-#     aridCARTdat$IUCNstatus == "Endangered" |
-#       aridCARTdat$IUCNstatus == "Extinct" |
-#       aridCARTdat$IUCNstatus == "Critically Endangered" |
-#       aridCARTdat$IUCNstatus == "Extinct in the Wild" |
-#       aridCARTdat$IUCNstatus == "Near Threatened" |
-#       aridCARTdat$IUCNstatus == "Vulnerable",
-#     1,
-#     0
-#   )
-
-
-## build a baby model
-TheModel <- rpart(dangerfish ~
-                   endemic +
-                  AUSnative +
-                  USAnative +
-                  maxtl_cm +
-                  spawning + 
-                  # slowcurr +
-                  # modcurr +
-                  # fastcurr +
-                  # algphyto +
-                  # macvascu +
-                  # detritus + 
-                  # invlvfsh +
-                  # fshcrcrb +
-                  FoodTroph.x,
-                 dat = aridCARTdat
+ggsave(
+  device = "png",
+  plot = barplot,
+  filename = "figures/IUCN_v_arid.png",
+  height = 4,
+  width = 9,
+  dpi = 600
 )
-prp(TheModel, yesno = 2, type = 2, digits = 4, extra = 1)
-# plot(TheModel)
-# text(TheModel, digits = 3)
-print(TheModel, digits = 2)
 
-# do we need to prune?
-plotcp(TheModel) 
-
-# ggsave(CARTplot, filename = "figures/CARTmodel.png", dpi = 300, height = 7, width = 7)
+# ## CART model is meh ----
+# # modeled after this: https://repositories.lib.utexas.edu/bitstream/handle/2152/94746/Perkin%20et%20al%202021.pdf?sequence=3
+# library(tree)
+# library(rpart)
+# library(rpart.plot)
+# 
+# # first filter data based on columns of interest
+# names(allarid)
+# 
+# aridCARTdat <- select(allarid, 
+#                       c("GenusSpecies",
+#                       "habitat",
+#                       "location",
+#                       "endemic",
+#                       "AUSnative",
+#                       "USAnative",
+#                       "nonfeed",
+#                       "algphyto",
+#                       "macvascu",
+#                       "detritus",
+#                       "invlvfsh",
+#                       "fshcrcrb",
+#                       "blood",
+#                       "eggs",
+#                       "maxtl_cm",
+#                       "guarder",
+#                       "open.substratum.spawner",
+#                       "broodhider",
+#                       "livebearers",
+#                       "sprgsubt",
+#                       "lacustrine",
+#                       "potanadr",
+#                       "slowcurr",
+#                       "modcurr",
+#                       "fastcurr",
+#                       "IUCNstatus",
+#                       "FoodTroph.x"))
+# str(aridCARTdat)
+# 
+# # make many of these columns factors instead
+# cols <- c("location", "endemic", "AUSnative", "USAnative",
+#           "algphyto", "macvascu", "detritus", "invlvfsh",
+#           "fshcrcrb", "blood", "eggs", "guarder", "open.substratum.spawner",
+#           "broodhider", "livebearers", "sprgsubt", "lacustrine",
+#           "potanadr", "slowcurr", "modcurr", "fastcurr")
+# 
+# aridCARTdat %<>%
+#   mutate_each_(funs(as.factor(.)),cols)
+# # aridCARTdat %<>%
+# #   mutate_each_(funs(as.logical(.)),cols)
+# str(aridCARTdat)
+# summary(aridCARTdat) # all looks good
+# 
+# # ## to make CART ready elimate NAs
+# aridCARTdat <- na.omit(aridCARTdat)
+# 
+# ## melt dataframe for food, current, and spawning
+# # made a clunky ifelse statement because that's how I am
+# aridCARTdat$spawning <- (ifelse(aridCARTdat$open.substratum.spawner == TRUE, "open.substratum",
+#                                ifelse(aridCARTdat$broodhider == TRUE, "broodhider",
+#                                       ifelse(aridCARTdat$livebearers == TRUE, "livebearers",
+#                                              ifelse(aridCARTdat$guarder == TRUE, "guarder",
+#                                                     NA)))))
+# aridCARTdat$spawning <- as.factor(aridCARTdat$spawning)
+#                     
+# 
+# 
+# ## make a new column for species in trouble
+# setDT(aridCARTdat)
+# aridCARTdat$dangerfish <-
+#   aridCARTdat[, dangerfish := ifelse(IUCNstatus %in%
+#                                        c("Endangered", 
+#                                          "Extinct",
+#                                          "Critically Endangered",
+#                                          "Extinct in the Wild",
+#                                          "Near Threatened",
+#                                          "Vulnerable"), 1, 0)]
+# 
+# # aridCARTdat$dangerfish <-
+# #   ifelse(
+# #     aridCARTdat$IUCNstatus == "Endangered" |
+# #       aridCARTdat$IUCNstatus == "Extinct" |
+# #       aridCARTdat$IUCNstatus == "Critically Endangered" |
+# #       aridCARTdat$IUCNstatus == "Extinct in the Wild" |
+# #       aridCARTdat$IUCNstatus == "Near Threatened" |
+# #       aridCARTdat$IUCNstatus == "Vulnerable",
+# #     1,
+# #     0
+# #   )
+# 
+# 
+# ## build a baby model
+# TheModel <- rpart(dangerfish ~
+#                    endemic +
+#                   AUSnative +
+#                   USAnative +
+#                   maxtl_cm +
+#                   spawning + 
+#                   # slowcurr +
+#                   # modcurr +
+#                   # fastcurr +
+#                   # algphyto +
+#                   # macvascu +
+#                   # detritus + 
+#                   # invlvfsh +
+#                   # fshcrcrb +
+#                   FoodTroph.x,
+#                  dat = aridCARTdat
+# )
+# prp(TheModel, yesno = 2, type = 2, digits = 4, extra = 1)
+# # plot(TheModel)
+# # text(TheModel, digits = 3)
+# print(TheModel, digits = 2)
+# 
+# # do we need to prune?
+# plotcp(TheModel) 
+# 
+# # ggsave(CARTplot, filename = "figures/CARTmodel.png", dpi = 300, height = 7, width = 7)
 
 
